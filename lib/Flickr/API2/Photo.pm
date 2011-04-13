452149a53bbc0e34a4ce1283e7982639f3d0243f
Toby Corkindale <tjc@cpan.org>
2011-04-13 12:42:46 +1000

    Add more documentation to Photos; add short_url method.

--- lib/Flickr/API2/Photo.pm
+++ lib/Flickr/API2/Photo.pm
@@ -43,7 +43,14 @@ has 'url_l' => ( is => 'rw' );
 has 'description' => ( is => 'rw' );
 has 'path_alias' => ( is => 'rw' );
 
-# Incomplete/untested
+=head1 METHODS
+
+=head2 info
+
+Returns getInfo results for this photo; not really tested much yet.
+
+=cut
+
 sub info {
     my ($self) = shift;
     my $response = $self->api->execute_method(
@@ -54,6 +61,12 @@ sub info {
     );
     return $response;
 }
+
+=head2 sizes
+
+Returns getSizes results for this photo.
+
+=cut
 
 sub sizes {
     my ($self) = shift;
@@ -66,13 +79,34 @@ sub sizes {
     return $response->{sizes};
 }
 
-# Doesn't quite work right yet.
+=head2 page_url
+
+Returns the URL for this photo's page on Flickr.
+
+=cut
+
+# See http://www.flickr.com/services/api/misc.urls.html
+# for generation method
 sub page_url {
     my $self = shift;
     return sprintf('http://flickr.com/photos/%s/%d',
         ($self->path_alias || $self->owner_name || $self->owner_id),
         $self->id
     );
+}
+
+=head2 short_url
+
+Returns the shortened URL for the photo page, ie. at http://flic.kr/
+
+NOT YET IMPLEMENTED
+
+=cut
+
+sub short_url {
+    my $self = shift;
+    # TODO: implement base58 encoding of the id
+    sprintf('http://flic.kr/p/%s', $self->id);
 }
 
 __PACKAGE__->meta->make_immutable;
